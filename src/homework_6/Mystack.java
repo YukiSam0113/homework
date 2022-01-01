b1e50fa4641c1c4a2d21871e53123f3a33a36781
YukiSam0113 <91315163+YukiSam0113@users.noreply.github.com>
2022-01-02 06:54:09 +0800

    Update Mystack.java

--- src/homework_6/Mystack.java
+++ src/homework_6/Mystack.java
@@ -1,10 +1,35 @@
 package homework_6;
 
+import java.util.ArrayList;
+
 public class Mystack {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	private ArrayList<Object> list=new ArrayList<>(); 
+	public boolean isEmpty() 
+	{ 
+		return list.isEmpty(); 
 	}
-
+	public int getSize() 
+	{ 
+		return list.size(); 
+	}
+	public Object peek() 
+	{ 
+		return list.get(getSize()-1);
+	}
+	public Object pop() 
+	{
+		Object o=list.get(getSize()-1);
+		list.remove(getSize()-1); 
+		return o; 
+	}
+	public void push(Object o) 
+	{ 
+		list.add(o); 
+	}
+	@Override 
+	public String toString()
+	{ 
+		return "Stack: "+list.toString();
+	}
 }
