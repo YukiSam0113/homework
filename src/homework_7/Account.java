e39456a5368353d2b5645d389edd624dce1a7a29
YukiSam0113 <91315163+YukiSam0113@users.noreply.github.com>
2022-01-02 07:00:39 +0800

    Update Account.java

--- src/homework_7/Account.java
+++ src/homework_7/Account.java
@@ -12,11 +12,11 @@ public class Account {
 		account.deposit(5);
 		account.deposit(4);
 		account.deposit(2);
-		System.out.println("�˻�������"+account.getName());
-		System.out.println("�����ʣ�"+Account1.getAnnualInterestRate());
-		System.out.println("��"+account.getBalance());
+		System.out.println("账户姓名："+account.getName());
+		System.out.println("年利率："+Account1.getAnnualInterestRate());
+		System.out.println("余额："+account.getBalance());
 		java.util.ArrayList<Transaction> list=account.getTransactions();
-		System.out.printf("%-30s%-14s%-14s%-15s\n","����ʱ��", "��������", "���׽��", "�˻����");
+		System.out.printf("%-30s%-14s%-14s%-15s\n","交易时间", "交易类型", "交易金额", "账户余额");
 		for(int i=0;i<list.size();i++) {
 			Transaction transaction = (Transaction) (list.get(i));
 			System.out.printf("%-35s%-14s%-16s%-15s\n",transaction.getDate(),transaction.getType(),
@@ -102,11 +102,11 @@ class Account1{
 	}
 	public void withdraw(double amount) {
 		balance-=amount;
-		transactions.add(new Transaction("ȡ��",amount,balance,""));
+		transactions.add(new Transaction("取款",amount,balance,""));
 	}
 	public void deposit(double amount) {
 		balance+=amount;
-		transactions.add(new Transaction("���",amount,balance,""));
+		transactions.add(new Transaction("存款",amount,balance,""));
 	}
 }
 
